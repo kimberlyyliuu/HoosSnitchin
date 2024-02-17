dbe912710713a11d400c2cb5a7ce0364eec8bfb2
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2024-02-17 16:23:47 -0500

    remove secret

--- init_site/settings.py
+++ init_site/settings.py
@@ -146,8 +146,8 @@ SITE_ID = 1
 SOCIALACCOUNT_PROVIDERS = {
     "google": {
         "APP": {
-            "client_id": "428903485592-lbrrbl2mp3lmtoe6s79apvc8h8927ob2.apps.googleusercontent.com",
-            "secret": "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",
+            "client_id": "",
+            "secret": "",
             "key": "",
         }
     }

5d178cc8bf4afeb4f8a2df179887471a4f52adf7
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2024-03-16 16:31:09 -0400

    made tests run on all branches and linted tests file

--- main/tests.py
+++ main/tests.py
@@ -1,121 +1,122 @@
-from django.shortcuts import redirect, render
-from django.views import generic
 from main.models import CustomUser, Document, Report, Event
-from django.contrib.auth import login, logout
-from django.http import JsonResponse
-from django.shortcuts import render, redirect
-from main.forms import ReportForm, DocumentForm
 from django.test import TestCase
 
-
-# Create your tests here.
 
 # Test Set 1: Test the CustomUser model
 class CustomUserModelTests(TestCase):
     # Test 1-1: Creation
     def test_creation(self):
         user = CustomUser.objects.create_user(
-            email='test@example.com',
-            username='testuser',
-            password='testpassword',
-            first_name='Kevin',
-            last_name='Cha'
+            email="test@example.com",
+            username="testuser",
+            password="testpassword",
+            first_name="Kevin",
+            last_name="Cha",
         )
-        self.assertEqual(user.email, 'test@example.com')
-        self.assertEqual(user.username, 'testuser')
-        self.assertEqual(user.first_name, 'Kevin')
-        self.assertEqual(user.last_name, 'Cha')
-        self.assertTrue(user.check_password('testpassword'))
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.first_name, "Kevin")
+        self.assertEqual(user.last_name, "Cha")
+        self.assertTrue(user.check_password("testpassword"))
         self.assertFalse(user.is_site_admin)
         self.assertTrue(user.is_active)
+
 
 # Test Set 2: Test the Report model
 class ReportModelTests(TestCase):
     # Test 2-1: Creation
     def test_creation_ReportModelTests(self):
-        user = CustomUser.objects.create_user(email='test@example.com', username='testuser', password='testpassword', first_name='Kevin', last_name='Cha')
-        event = Event.objects.create(title='Test Event', description='Test Description')
-        report = Report.objects.create(
-            description='Test Report', 
-            user_id=1, 
-            event_id=1
+        user = CustomUser.objects.create_user(
+            email="test@example.com",
+            username="testuser",
+            password="testpassword",
+            first_name="Kevin",
+            last_name="Cha",
         )
-        self.assertEqual(report.description, 'Test Report')
+        event = Event.objects.create(title="Test Event", description="Test Description")
+        report = Report.objects.create(description="Test Report", user_id=1, event_id=1)
+        self.assertEqual(report.description, "Test Report")
         self.assertEqual(report.user_id, 1)
         self.assertEqual(report.event_id, 1)
-    
+
 
 # Test Set 3: Test the Document model
 class DocumentModelTests(TestCase):
     # Test 3-1: Creation
     def test_creation_DocumentModelTests(self):
         document = Document.objects.create(
-            document='Test Document', 
-            created_at='2024-01-01 00:10:00', 
-            title='Test Title'
+            document="Test Document",
+            created_at="2024-01-01 00:10:00",
+            title="Test Title",
         )
-        self.assertEqual(document.document, 'Test Document')
-        self.assertEqual(document.created_at, '2024-01-01 00:10:00')
-        self.assertEqual(document.title, 'Test Title')
+        self.assertEqual(document.document, "Test Document")
+        self.assertEqual(document.created_at, "2024-01-01 00:10:00")
+        self.assertEqual(document.title, "Test Title")
+
 
 # Test Set 4: Test the Event model
 class EventModelTests(TestCase):
     # Test 4-1: Creation
     def test_creation_EventModelTests(self):
-        event = Event.objects.create(
-            title='Test Event', 
-            description='Test Description'
-        )
-        self.assertEqual(event.title, 'Test Event')
-        self.assertEqual(event.description, 'Test Description')
+        event = Event.objects.create(title="Test Event", description="Test Description")
+        self.assertEqual(event.title, "Test Event")
+        self.assertEqual(event.description, "Test Description")
+
 
 # Test Set 5: Test Views
 class ViewsTests(TestCase):
     # Test 5-1: Test the index view Basic
     def test_index_view(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main/index.html')
-    
+        self.assertTemplateUsed(response, "main/index.html")
+
     # Test 5-2: Test the document upload view Basic
     def test_document_upload_view(self):
-        response = self.client.get('/report/1/upload')
+        response = self.client.get("/report/1/upload")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main/document_upload.html')
+        self.assertTemplateUsed(response, "main/document_upload.html")
 
     # Test 5-3: Test the report upload view Basic
     def test_report_upload_view(self):
-        response = self.client.get('/report/')
+        response = self.client.get("/report/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main/report_upload.html')
+        self.assertTemplateUsed(response, "main/report_upload.html")
 
     # Test 5-4: Test the logout view Basic
     def test_logout_view(self):
-        response = self.client.post('/logout/')
+        response = self.client.post("/logout/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "Logged out successfully"})
+
 
 # Test Set 6: Test Forms
 class FormsTests(TestCase):
     # Test 6-1: Test the report form
     def test_report_form(self):
-        user = CustomUser.objects.create_user(email='test@example.com', username='testuser', password='testpassword', first_name='Kevin', last_name='Cha')
-        event = Event.objects.create(title='Test Event', description='Test Description')
-        
-        response = self.client.post('/report/', {
-        'description': 'Test Report Description',
-        'event': event.title
-        })
+        user = CustomUser.objects.create_user(
+            email="test@example.com",
+            username="testuser",
+            password="testpassword",
+            first_name="Kevin",
+            last_name="Cha",
+        )
+        event = Event.objects.create(title="Test Event", description="Test Description")
+
+        response = self.client.post(
+            "/report/", {"description": "Test Report Description", "event": event.title}
+        )
         self.assertEqual(response.status_code, 200)
-        #self.assertEqual(response.context['form'].cleaned_data['description'], 'Test Report Description')
+        # self.assertEqual(response.context['form'].cleaned_data['description'], 'Test Report Description')
 
     # Test 6-2: Test the document form
     def test_document_form(self):
-        response = self.client.post('/report/1/upload', {'file_field': 'Test Document'})
+        response = self.client.post("/report/1/upload", {"file_field": "Test Document"})
         self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response.context['form'].cleaned_data['file_field'], 'Test Document')
+        # self.assertEqual(response.context['form'].cleaned_data['file_field'], 'Test Document')
 
-'''
+
+"""
 # Test Set X: Test Login/Logout functionality
 class AccessControlTests(TestCase):
     # Test 2-1: Test login
@@ -131,4 +132,4 @@ class AccessControlTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['user'].firstname, 'Kevin')
         self.assertTrue(response.context['user'].is_authenticated)
-'''
+"""

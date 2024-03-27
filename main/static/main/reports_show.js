c1ade8d325da17d80594ae6b74691b15a39adb0d
Kevin Cha <143443005+kevincha12@users.noreply.github.com>
2024-03-27 01:54:57 -0400

    put function in its own js

---
+++ main/static/main/reports_show.js
@@ -0,0 +1,11 @@
+// REFERENCES
+// TITLE: How To Toggle Hide/Show using JS
+// URL: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+function reports_show(report_id){
+        var report_content = document.getElementById(report_id);
+        if(report_content.style.display === "none")
+            report_content.style.display = "flex";
+        else
+            report_content.style.display = "none";
+    }

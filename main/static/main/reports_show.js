33e5ef601094c286e970b499d51f9d0a36336265
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2024-04-07 17:30:30 -0400

    small changes to admin view to get navbar and footer correct. otherwise leaving how is for now.

--- main/static/main/reports_show.js
+++ main/static/main/reports_show.js
@@ -2,21 +2,21 @@
 // TITLE: How To Toggle Hide/Show using JS
 // URL: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 
-function reports_show(report_id){
-    var report_content = document.getElementById(report_id);
-    if(report_content.style.display === "none"){
-        report_content.style.display = "flex";
-        $.ajax({
-            url: '/update_report/',  
-            type: 'post',
-            data: {
-                'report_id': report_id,
-            },
-            success: function(response) {
-                console.log(response);
-            }
-        });
-    }else{
-        report_content.style.display = "none";
-    }
+function reports_show(report_id) {
+  var report_content = document.getElementById(report_id);
+  if (report_content.style.display === "none") {
+    report_content.style.display = "flex";
+    $.ajax({
+      url: "/update_report/",
+      type: "post",
+      data: {
+        report_id: report_id,
+      },
+      success: function (response) {
+        console.log(response);
+      },
+    });
+  } else {
+    report_content.style.display = "none";
+  }
 }
